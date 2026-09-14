fe3049563e9c9b3ec7ac8a9bd951af8067a23d8c
Borja Garibotti <mgaribotti@fi.uba.ar>
2026-09-14 20:34:28 -0300

    refactor: extract duplicated logic to _RabbitMQBase

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,4 +1,4 @@
-# ruff: noqa: BLE001, S110
+# ruff: noqa: BLE001
 import pika
 
 from .middleware import (
@@ -10,168 +10,85 @@ from .middleware import (
 )
 
 
-def _raise(exception: Exception):
-    if isinstance(
-        exception,
-        (
-            MessageMiddlewareCloseError,
-            MessageMiddlewareDisconnectedError,
-            MessageMiddlewareMessageError,
-        ),
-    ):
-        raise exception
-
-    if isinstance(exception, pika.exceptions.AMQPConnectionError):
-        raise MessageMiddlewareDisconnectedError(exception) from exception
-
-    if isinstance(exception, pika.exceptions.AMQPChannelError):
-        raise MessageMiddlewareMessageError(exception) from exception
-
-    raise MessageMiddlewareMessageError(exception) from exception
-
-
-def _transform(on_message_callback):
-    def callback(channel, method, _properties, body):
-        tag = method.delivery_tag
-        on_message_callback(
-            message=body,
-            ack=lambda: channel.basic_ack(delivery_tag=tag),
-            nack=lambda: channel.basic_nack(
-                delivery_tag=tag,
-                requeue=False,
-            ),
-        )
-
-    return callback
-
-
-class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
-    def __init__(self, host, queue_name):
+class _RabbitMQBase:
+    def __init__(self, host):
+        self._consuming = False
         self._channel = None
         self._connection = None
-
         try:
-            self._consuming = False
             self._connection = pika.BlockingConnection(
                 pika.ConnectionParameters(host=host),
             )
             self._channel = self._connection.channel()
-            self._queue_name = queue_name
+        except Exception as e:
+            self._handle_exception(e)
+
+    def _assert_connection(self):
+        try:
+            if self._connection and self._connection.is_open:
+                return
+        except Exception as e:
+            raise MessageMiddlewareDisconnectedError(e) from e
+        raise MessageMiddlewareDisconnectedError()
+
+    def _close(self, raise_error):
+        error = None
+        try:
+            if self._channel and self._channel.is_open:
+                self._channel.close()
+        except Exception as e:
+            error = error or e
+
+        try:
+            if self._connection and self._connection.is_open:
+                self._connection.close()
+        except Exception as e:
+            error = error or e
+
+        self._channel = None
+        self._connection = None
+        if error and raise_error:
+            raise MessageMiddlewareCloseError(error) from error
+
+    def _handle_exception(self, exception):
+        self._close(raise_error=False)
+        self._raise(exception)
+
+    def _raise(self, exception: Exception):
+        if isinstance(
+            exception,
+            (
+                MessageMiddlewareCloseError,
+                MessageMiddlewareDisconnectedError,
+                MessageMiddlewareMessageError,
+            ),
+        ):
+            raise exception
+
+        if isinstance(exception, pika.exceptions.AMQPConnectionError):
+            raise MessageMiddlewareDisconnectedError(exception) from exception
+
+        if isinstance(exception, pika.exceptions.AMQPChannelError):
+            raise MessageMiddlewareMessageError(exception) from exception
+
+        raise MessageMiddlewareMessageError(exception) from exception
+
+    def setup_queue(self, queue_name):
+        try:
+            self._assert_connection()
             self._channel.queue_declare(
-                queue=self._queue_name,
+                queue=queue_name,
                 durable=False,
                 auto_delete=False,
             )
         except Exception as e:
             self._handle_exception(e)
 
-    def _handle_exception(self, exception):
+    def setup_exchange(self, exchange_name, routing_keys):
         try:
-            self._close()
-        except Exception:
-            pass
-        _raise(exception)
-
-    def _close(self):
-        error = None
-        if self._channel and self._channel.is_open:
-            try:
-                self._channel.close()
-            except Exception as e:
-                error = e
-            finally:
-                self._channel = None
-        if self._connection and self._connection.is_open:
-            try:
-                self._connection.close()
-            except Exception as e:
-                error = error or e
-            finally:
-                self._connection = None
-        if error:
-            raise error
-
-    def _assert_connection(self):
-        if not self._connection or not self._connection.is_open:
-            raise MessageMiddlewareDisconnectedError()
-
-    # Comienza a escuchar a la cola/exchange e invoca a on_message_callback tras
-    # cada mensaje de datos o de control con el cuerpo del mensaje.
-    # on_message_callback tiene como parámetros:
-    # message - El valor tal y como lo recibe el método send de esta clase.
-    # ack - Función que al invocarse realiza ack al mensaje que se está consumiendo.
-    # nack - Función que al invocarse realiza nack al mensaje que se está consumiendo.
-    # Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
-    # Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
-    def start_consuming(self, on_message_callback):
-        self._assert_connection()
-        if self._consuming:
-            return
-
-        try:
-            self._consuming = True
-
-            self._channel.basic_consume(
-                queue=self._queue_name,
-                on_message_callback=_transform(on_message_callback),
-                auto_ack=False,
-            )
-            self._channel.start_consuming()
-        except Exception as e:
-            self._handle_exception(e)
-        finally:
-            self._consuming = False
-
-    # Si se estaba consumiendo desde la cola/exchange, se detiene la escucha. Si
-    # no se estaba consumiendo de la cola/exchange, no tiene efecto, ni levanta
-    # Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
-    def stop_consuming(self):
-        self._assert_connection()
-        try:
-            if self._consuming:
-                self._channel.stop_consuming()
-                self._consuming = False
-        except Exception as e:
-            self._handle_exception(e)
-
-    # Envía un mensaje a la cola o al tópico con el que se inicializó el exchange.
-    # Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
-    # Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
-    def send(self, message):
-        self._assert_connection()
-        try:
-            self._channel.basic_publish(
-                exchange="",
-                routing_key=self._queue_name,
-                body=message,
-            )
-        except Exception as e:
-            self._handle_exception(e)
-
-    # Se desconecta de la cola o exchange al que estaba conectado.
-    # Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareCloseError.
-    def close(self):
-        try:
-            self._close()
-        except Exception as e:
-            raise MessageMiddlewareCloseError(e) from e
-
-
-class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
-    def __init__(self, host, exchange_name, routing_keys):
-        self._channel = None
-        self._connection = None
-
-        try:
-            self._consuming = False
-            self._connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=host),
-            )
-            self._channel = self._connection.channel()
-            self._exchange_name = exchange_name
-
+            self._assert_connection()
             self._channel.exchange_declare(
-                exchange=self._exchange_name,
+                exchange=exchange_name,
                 exchange_type="direct",
                 durable=False,
             )
@@ -182,47 +99,17 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
                 exclusive=True,
                 auto_delete=True,
             )
-            self._queue_name = queue.method.queue
+            queue_name = queue.method.queue
 
-            self._routing_keys = routing_keys
-            for routing_key in self._routing_keys:
+            for routing_key in routing_keys:
                 self._channel.queue_bind(
-                    queue=self._queue_name,
-                    exchange=self._exchange_name,
+                    queue=queue_name,
+                    exchange=exchange_name,
                     routing_key=routing_key,
                 )
+            return queue_name
         except Exception as e:
             self._handle_exception(e)
-
-    def _handle_exception(self, exception):
-        try:
-            self._close()
-        except Exception:
-            pass
-        _raise(exception)
-
-    def _close(self):
-        error = None
-        if self._channel and self._channel.is_open:
-            try:
-                self._channel.close()
-            except Exception as e:
-                error = e
-            finally:
-                self._channel = None
-        if self._connection and self._connection.is_open:
-            try:
-                self._connection.close()
-            except Exception as e:
-                error = error or e
-            finally:
-                self._connection = None
-        if error:
-            raise error
-
-    def _assert_connection(self):
-        if not self._connection or not self._connection.is_open:
-            raise MessageMiddlewareDisconnectedError()
 
     # Comienza a escuchar a la cola/exchange e invoca a on_message_callback tras
     # cada mensaje de datos o de control con el cuerpo del mensaje.
@@ -232,17 +119,28 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     # nack - Función que al invocarse realiza nack al mensaje que se está consumiendo.
     # Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
     # Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
-    def start_consuming(self, on_message_callback):
+    def start_consuming(self, on_message_callback, queue_name):
         self._assert_connection()
         if self._consuming:
             return
 
+        self._consuming = True
         try:
-            self._consuming = True
+
+            def callback(channel, method, _properties, body):
+                tag = method.delivery_tag
+                on_message_callback(
+                    message=body,
+                    ack=lambda: channel.basic_ack(delivery_tag=tag),
+                    nack=lambda: channel.basic_nack(
+                        delivery_tag=tag,
+                        requeue=False,
+                    ),
+                )
 
             self._channel.basic_consume(
-                queue=self._queue_name,
-                on_message_callback=_transform(on_message_callback),
+                queue=queue_name,
+                on_message_callback=callback,
                 auto_ack=False,
             )
             self._channel.start_consuming()
@@ -266,12 +164,12 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     # Envía un mensaje a la cola o al tópico con el que se inicializó el exchange.
     # Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
     # Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
-    def send(self, message):
+    def send(self, message, exchange_name, routing_keys):
         self._assert_connection()
         try:
-            for routing_key in self._routing_keys:
+            for routing_key in routing_keys:
                 self._channel.basic_publish(
-                    exchange=self._exchange_name,
+                    exchange=exchange_name,
                     routing_key=routing_key,
                     body=message,
                 )
@@ -281,7 +179,60 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     # Se desconecta de la cola o exchange al que estaba conectado.
     # Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareCloseError.
     def close(self):
-        try:
-            self._close()
-        except Exception as e:
-            raise MessageMiddlewareCloseError(e) from e
+        self._close(raise_error=True)
+
+
+class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
+    def __init__(self, host, queue_name):
+        self._base = _RabbitMQBase(host=host)
+        self._queue_name = queue_name
+        self._base.setup_queue(queue_name)
+
+    def start_consuming(self, on_message_callback):
+        self._base.start_consuming(
+            on_message_callback=on_message_callback,
+            queue_name=self._queue_name,
+        )
+
+    def stop_consuming(self):
+        self._base.stop_consuming()
+
+    def send(self, message):
+        self._base.send(
+            message=message,
+            exchange_name="",
+            routing_keys=[self._queue_name],
+        )
+
+    def close(self):
+        self._base.close()
+
+
+class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
+    def __init__(self, host, exchange_name, routing_keys):
+        self._base = _RabbitMQBase(host=host)
+        self._exchange_name = exchange_name
+        self._routing_keys = routing_keys
+        self._queue_name = self._base.setup_exchange(
+            exchange_name,
+            routing_keys,
+        )
+
+    def start_consuming(self, on_message_callback):
+        self._base.start_consuming(
+            on_message_callback=on_message_callback,
+            queue_name=self._queue_name,
+        )
+
+    def stop_consuming(self):
+        self._base.stop_consuming()
+
+    def send(self, message):
+        self._base.send(
+            message=message,
+            exchange_name=self._exchange_name,
+            routing_keys=self._routing_keys,
+        )
+
+    def close(self):
+        self._base.close()
